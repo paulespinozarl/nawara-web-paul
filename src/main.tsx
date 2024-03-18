33667bf6882dbfd2d4184c12f550c94cc8cc10b5
paulespinnozarl <paulespinozarl@gmail.com>
2024-03-18 13:09:34 -0300

    hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { App } from "./App.tsx";
+import { HashRouter } from "react-router-dom";
 
 import "./styles/index.css";
 import "./styles/burger.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
